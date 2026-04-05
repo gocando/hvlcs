4f19adc6546f3c90b1ec5902d74eb65839276e39
Grecia Ocando <greciaocando@gocando.local>
2026-04-05 16:06:17 -0400

    Added backtrack to find actual solution

--- HVLCS.py
+++ HVLCS.py
@@ -9,7 +9,20 @@ def hvlcs(A, B, val):
                 M[i][j] = max(M[i-1][j], M[i][j-1])
     return M
 
+def backtrack(M, A, B):
+    seq, i, j = [], len(A), len(B)
+    while i > 0 and j > 0:
+        if A[i-1] == B[j-1]:
+            seq.append(A[i-1])
+            i -= 1; j -= 1
+        elif M[i-1][j] >= M[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    return ''.join(reversed(seq))
+
 val = {'e': 2, 'r': 4, 'g': 6}
 A, B = "gree", "reger"
 M = hvlcs(A, B, val)
 print(M[len(A)][len(B)])
+print(backtrack(M, A, B))
