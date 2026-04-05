685f5d605741d224fe96c476ab02081742d74a85
gocando <gocandotiniacos@ufl.edu>
2026-04-05 15:47:42 -0400

    implemented main hvlcs DP function

---
+++ HVLCS.py
@@ -0,0 +1,15 @@
+def hvlcs(A, B, val):
+    m, n = len(A), len(B)
+    M = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if A[i-1] == B[j-1]:
+                M[i][j] = val[A[i-1]] + M[i-1][j-1]
+            else:
+                M[i][j] = max(M[i-1][j], M[i][j-1])
+    return M
+
+val = {'e': 2, 'r': 4, 'g': 6}
+A, B = "gree", "reger"
+M = hvlcs(A, B, val)
+print(M[len(A)][len(B)])
